2a24216fb3a22773261b4c853788744ce8274078
maximwolpher <maxim.wolpher@real-digital.de>
2018-12-22 02:05:05 +0100

    regex all the way

--- days/day_12/solution.py
+++ days/day_12/solution.py
@@ -1,3 +1,4 @@
+import re
 from input_reader import read
 
 data = read(__file__)
@@ -5,8 +6,30 @@ test_data = read(__file__, name='test')
 
 
 def parse(input_data):
-    return 0
+    init = input_data[0].rstrip().split(' ')[2]
+    reg_list = list()
+    for line in range(2, len(input_data)):
+        split_line = input_data[line].rstrip().split(' => ')
+        reg_list.append([re.compile(split_line[0]), split_line[1]])
+    return init, reg_list
 
 
-def task_one(input_data, grid_x, grid_y):
-    return 0
+def task_one(init: str, reg_list: list, gens:int) -> int:
+    init = '.' * 2 * gens + '.' * 2 + init
+    for idx in range(2, len(init)-2):
+        window = init[idx-2:idx+2]
+        did_match = False
+        for reg in reg_list:
+            if reg[0].search(window):
+                init = reg[0].sub(lambda m: m.group()[:2] + reg[1] + m.group()[3:], init)
+                did_match = True
+                break
+        if not did_match:
+            init = init[:]
+
+    return init
+
+
+parsed_test = parse(test_data)
+parsed_data = parse(data)
+print(task_one(*parsed_test, gens=20))
