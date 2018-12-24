01dcd16a90b3caaf5cefe06b38e1e1ac518a79bf
maximwolpher <maxim.wolpher@real-digital.de>
2018-12-24 01:14:53 +0100

    day 12 done

--- days/day_12/solution.py
+++ days/day_12/solution.py
@@ -10,26 +10,39 @@ def parse(input_data):
     reg_list = list()
     for line in range(2, len(input_data)):
         split_line = input_data[line].rstrip().split(' => ')
-        reg_list.append([re.compile(split_line[0]), split_line[1]])
+        reg_list.append([re.compile(re.escape(split_line[0])), split_line[1]])
     return init, reg_list
 
 
-def task_one(init: str, reg_list: list, gens:int) -> int:
-    init = '.' * 2 * gens + '.' * 2 + init
-    for idx in range(2, len(init)-2):
-        window = init[idx-2:idx+2]
-        did_match = False
-        for reg in reg_list:
-            if reg[0].search(window):
-                init = reg[0].sub(lambda m: m.group()[:2] + reg[1] + m.group()[3:], init)
-                did_match = True
-                break
-        if not did_match:
-            init = init[:]
+def task_one(init: str, reg_list: list, gens: int) -> list:
+    init = list('.' * 2 * gens + '.' * 2 + init + '.' * 2 + '.' * 2 * gens)
+    start_loc = 2 * gens + 2
+    all_scores = list()
+    for geneation in range(gens):
+        temp_list = ['.'] * len(init)
+        for idx in range(2, len(init) - 2):
+            window = ''.join(init[idx-2:idx+3])
+            for reg in reg_list:
+                wat = reg[0].match(window)
+                if wat:
+                    temp_list[idx] = reg[1]
+                    break
+        init = temp_list[:]
+        score = 0
+        for idx, value in enumerate(init):
+            if value == '#':
+                score += (idx - start_loc)
+        all_scores.append(score)
+    return all_scores
 
-    return init
+
+def task_two(input_data: list, gens: int) -> int:
+    diff = input_data[-1] - input_data[-2]
+    return input_data[-1] + diff * (gens - len(input_data))
 
 
 parsed_test = parse(test_data)
 parsed_data = parse(data)
-print(task_one(*parsed_test, gens=20))
+scores = task_one(*parsed_data, gens=200)
+print('Task one: ', scores[20-1])
+print('Task two: ', task_two(scores, gens=50000000000))
